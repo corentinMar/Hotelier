51b4f85d61edd17e6398aac5d8dfd674359e9167
Arnaud <Arnaud@Arnaud-Portable>
2015-01-21 11:15:02 +0100

    Rectification de l'url d'accès du Json.

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 var app = angular.module('main', ['ngTable']).
         controller('DemoCtrl', function ($scope, ngTableParams, $http) {
 
-            $http({method: 'GET', url: 'http://localhost/HotelierJson/public/hotel/listeHotelJson'}).
+            $http({method: 'GET', url: '/HotelierJson/public/hotel/listeHotelJson'}).
                     success(function (data) {
                         $scope.tableParams = new ngTableParams({
                             page: 1, // show first page
