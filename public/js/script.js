9e79d50c8f8eda2c39c3890aa4be2ed4c93ec5aa
Arnaud <Arnaud@192.168.56.1>
2015-01-21 11:00:37 +0100

    Json avec Pagination Fonctionnelle

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+var app = angular.module('main', ['ngTable']).
+        controller('DemoCtrl', function ($scope, ngTableParams, $http) {
+
+            $http({method: 'GET', url: 'http://localhost/HotelierJson/public/hotel/listeHotelJson'}).
+                    success(function (data) {
+                        $scope.tableParams = new ngTableParams({
+                            page: 1, // show first page
+                            count: 5 // count per page
+                        }, {
+                            total: data.length, // length of data
+                            getData: function ($defer, params) {
+                                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                            }
+                        });
+                    }).
+                    error(function (data) {
+                    });
+        });
+
+//Code par Arnaud
