c0c3433e959ecf436cfde73a7a58c5a732f61eed
Arnaud <Arnaud@Arnaud-Portable>
2015-01-22 17:09:42 +0100

    Ajout du tri Javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,25 @@
 var app = angular.module('main', ['ngTable']).
-        controller('DemoCtrl', function ($scope, ngTableParams, $http) {
+        controller('DemoCtrl', function ($scope, $filter, ngTableParams, $http) {
 
             $http({method: 'GET', url: '/HotelierJson/public/hotel/listeHotelJson'}).
                     success(function (data) {
                         $scope.tableParams = new ngTableParams({
                             page: 1, // show first page
-                            count: 5 // count per page
+                            count: 5, // count per page
+                            sorting: {
+                                idHotel: 'asc'     // initial sorting
+                            }
                         }, {
                             total: data.length, // length of data
                             getData: function ($defer, params) {
-                                $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                                var orderedData = params.sorting() ?
+                                        $filter('orderBy')(data, params.orderBy()) :
+                                        data;
+
+                                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
                             }
                         });
                     }).
                     error(function (data) {
                     });
         });
-
-//Arnaud Code Edit
